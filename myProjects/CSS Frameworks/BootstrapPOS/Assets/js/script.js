1b87592064da9d063f1f7db5c28ec6b50e56026c
Your Name <you@example.com>
2022-02-10 10:24:23 +0530

    navigate without refresh

---
+++ myProjects/CSS Frameworks/BootstrapPOS/Assets/js/script.js
@@ -0,0 +1,47 @@
+$("#customer").css("display","block");
+$("#item").css("display","none");
+$("#order").css("display","none");
+$("#details").css("display","none");
+
+$("#customerNav").click(function () {
+    $("#customer").css("display","block");
+    $("#item").css("display","none");
+    $("#order").css("display","none");
+    $("#details").css("display","none");
+});
+
+$("#itemNav").click(function () {
+    $("#customer").css("display","none");
+    $("#item").css("display","block");
+    $("#order").css("display","none");
+    $("#details").css("display","none");
+});
+
+$("#orderNav").click(function () {
+    $("#customer").css("display","none");
+    $("#item").css("display","none");
+    $("#order").css("display","block");
+    $("#details").css("display","none");
+});
+
+$("#detailsNav").click(function () {
+    $("#customer").css("display","none");
+    $("#item").css("display","none");
+    $("#order").css("display","none");
+    $("#details").css("display","block");
+});
+
+$("#btnSaveCustomer").click(function () {
+    let customerId = $("#txtCustId").val();
+    let customerName = $("#txtCustName").val();
+    let customerAddress = $("#txtCustAddress").val();
+    let customerSalary = $("#txtCustSalary").val();
+
+    let row = `<tr><td>${customerId}</td><td>${customerName}</td><td>${customerAddress}</td><td>${customerSalary}</td></tr>`;
+    $("#customerTable").append(row);
+
+    $("#customerTable>tr").click(function () {
+        console.log(this);
+    });
+
+})
